6472e558769e2d51ac5868c970cc085f2762e9f9
petar-Atanasov <PA673@live.mdx.ac.uk>
2024-07-10 16:53:14 +0100

    creating a hashfunction

---
+++ hashFun.py
@@ -0,0 +1,11 @@
+import hashlib, json, sys
+
+def hashMe(msg=""):
+    # this is a helper function that wraps our hashing algorithm
+    if type(msg)!=str:
+        msg = json.dumps(msg,sort_keys=True)
+    # sort keys to guarantee repeatability
+    
+    #encode the string to bytes and calculeate it
+    return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()        
+    
