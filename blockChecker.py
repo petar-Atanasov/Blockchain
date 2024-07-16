bdee374b9fbf4f9a5f059901f62f7b9b77116ba3
petar-Atanasov <PA673@live.mdx.ac.uk>
2024-07-16 01:04:38 +0100

    initial update

--- blockChecker.py
+++ blockChecker.py
@@ -1,6 +1,7 @@
-from hashFun import hashMe
-import copy 
+import copy
 
+from hashFun import hashMe 
+from stateUpdater import StateUpdater
 class BlockCheker:
     @staticmethod
        # helper function that makes sure that the block contents match the hash
@@ -25,10 +26,6 @@ class BlockCheker:
     # 3. Block number increments the parent block number by 1
     # 4. Accurately references the parent block's hash
     
-    # parentNumber = parent['contents']['blockNumber']
-    # parentHash = parent['hash']
-    # blockNumber = block['contents']['blockNumber']
-    
     # Check transaction validity:
     # throw and error if an invalid transaction was found 
    # print(f"Before transaction: {state}")
@@ -36,8 +33,8 @@ class BlockCheker:
         try:
             for txn in block['contents']['txns']:
             #print(f"Processing transaction {txn} with state {state}")
-                if isValid(txn,state): # type: ignore
-                    state = updateState(txn,state) # type: ignore
+                if StateUpdater.isValid(txn,state): # type: ignore
+                    state = StateUpdater.updateState(txn,state) # type: ignore
                 #print(f"After transaction {txn}: , the state is {state}")
                 else:
                     raise Exception(f'Invalid transaction in the block {block["contents"]["blockNumber"]}: {txn}') 
@@ -55,38 +52,4 @@ class BlockCheker:
             state = original_state
             raise e
     
-        return state
-    
-    @staticmethod
-    def isValid(txn, state):
-    # Assume that the transaction is a dictionary keyed by account names
-    
-    # Check that the sum of the deposit and withdrawls is -
-    # print(f"Checking transaction {txn} with current state {state}")
-        if sum(txn.values()) != 0:
-            return False
-    
-    # Check that the transaction does not cause an overdraft
-        for key, amount in txn.items():
-            current_balance = state.get(key, 0)
-            if current_balance + amount < 0:
-                return False
-        return True
-    
-    @staticmethod
-    def updateState(txn, state):
-    # Inputs: txn, state: dictinaries keyed with account names,
-    # holding numeric values for transfer amount (txn) or account
-    # balance (state)
-        
-    # Returns: Update state, with additional user added to state if necessary
-    # NOTE: This does not validate the transaction - just updates the state!
-        
-    # If the transaction is valid, then update the state
-        state = state.copy() # As dictionaries are mutable,
-    # let's avoid any confusion by creating a working
-    # copy of the data.
-        for key in txn:
-            if key in state.keys():
-                state[key] = state.get(key, 0) + txn[key]
-        return state 
+        return state    
